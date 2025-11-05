6c37fe29b9ca0f18013304925e39602ae16a173e
Shreyaskp123 <shreyaskp10@gmail.com>
2025-11-05 19:53:07 +0530

    Add files via upload

---
+++ kp2.py
@@ -0,0 +1,39 @@
+i=0
+while i<=10:
+    print("*"*i)
+    i+=1
+
+
+name="sshreyas"
+for letters in name:
+    print(letters,end=" ")
+
+
+
+for i in range(2,10+1):
+    for j in range(1,10+1):
+        print(f"{i}X{j}={i*j}")
+
+for i in range(1,10):
+    print(f"2x{i}={2*i}") 
+
+
+
+l=[5,1,3,4,2]
+total=0
+for sum in l:
+    total=total+sum
+    print(total)
+
+
+l=[1,23,43,534,32]
+dl=[]
+#print(dl)
+for num in l:
+    dl.append(sum*2)
+    print(dl)
+
+
+
+    
+
